68fe8efce5a41a596b4e2c26394be7177affa5a9
mozzie <joshuamutiso455@gmail.com>
2025-01-08 16:02:48 +0300

    Arrays

--- 02/02_08/02_08b/main.py
+++ 02/02_08/02_08b/main.py
@@ -1,2 +1,7 @@
 my_list = [1, 7, 3]
-print(sorted(my_list))
+print(sorted(my_list, reverse=True))
+
+
+student_grade = [('Sarah', 89), ('Rebecca', 82),('Matt', 91)]
+print(sorted(student_grade))
+print(sorted(student_grade, key=lambda x:x[1], reverse=True))
